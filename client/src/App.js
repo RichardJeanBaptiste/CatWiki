e23b8840cb9e0a9576d7bdccb4d49f02b0d7cf9e
Richard Jean-Baptiste <Richinbk1@gmail.com>
2021-09-07 18:12:11 -0400

    working on the ui

--- client/src/App.js
+++ client/src/App.js
@@ -1,38 +1,70 @@
-import logo from './logo.svg';
 import './App.css';
-import {React, useState, useEffect} from 'react';
+import {React} from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import { Box, SvgIcon, Typography } from '@material-ui/core';
+import Icon from '@material-ui/core/Icon';
+import CatWikiSVG from './assets/CatwikiLogo.svg';
 
-function App() {
 
-  const [ intro, setIntro ] = useState("");
 
-  useEffect(() => {
+const useStyles = makeStyles({
+  root: {
+    position: 'absolute',
+    left: '50%',
+    top: '60%',
+    borderRadius: '42px',
+    width: '90%',
+    height: '90%',
+    background: '#050709',
+    transform: 'translate(-50%, -50%)',
+  },
+  upperRoot: {
+    width:'90%',
+    height: '45%',
+  },
+  imageIcon: {
+    height: '75px',
+  },
+  iconRoot: {
+    textAlign: 'center',
+  }
 
-    fetch('/hello')
-      .then(response => response.text())
-      .then(data => setIntro(data))
+})
 
-  },[])
-  
+function CatIcon() {
+  const classes = useStyles();
 
   return (
-    <div className="App">
-      <header className="App-header">
-        {intro}
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Icon className={classes.iconRoot} >
+      <img className={classes.imageIcon} src={CatWikiSVG} alt="cat logo"/>
+    </Icon>
+  )
+}
+
+function HomeIcon(props) {
+  return (
+    <SvgIcon {...props}>
+      <path d="M120.65 25.886L119.61 29.0067L118.57 32.9596C118.57 32.9596 115.865 34.416 115.657 33.3757C115.449 32.3355 116.697 32.1274 116.697 32.1274L117.113 30.047L113.993 24.4297L120.65 25.886Z" fill="#291507"/>
+    </SvgIcon>
+  );
+}
+
+function App() {
+  
+  const classes = useStyles();
+
+  return (
+    <Box className={classes.root}>
+
+      <Box>
+          <HomeIcon style={{fontSize: '100px', color:'white'}}/>
+          <Typography variant="p">Get to know more about cat breed</Typography>
+      </Box>
+
+      <Box>
+
+      </Box>
+    </Box>
   );
 }
 
--- server.py
+++ server.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "<h1>Hello, World</h1>"
+    return 'abcd'
 
 @app.route("/hello")
 def connect():
