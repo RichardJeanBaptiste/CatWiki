2a84b800695088c7a65f33f0b218a47250ea6af4
Richard Jean-Baptiste <Richinbk1@gmail.com>
2021-09-07 20:55:16 -0400

    added svg icon

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,9 @@
 import './App.css';
 import {React} from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import { Box, SvgIcon, Typography } from '@material-ui/core';
-import Icon from '@material-ui/core/Icon';
-import CatWikiSVG from './assets/CatwikiLogo.svg';
+import { Box,Typography } from '@material-ui/core';
+import HeroImageLg from './assets/HeroImagemd.png';
+import CatWiki from './components/CatWikiIcon';
 
 
 
@@ -15,12 +15,15 @@ const useStyles = makeStyles({
     borderRadius: '42px',
     width: '90%',
     height: '90%',
-    background: '#050709',
     transform: 'translate(-50%, -50%)',
   },
   upperRoot: {
     width:'90%',
-    height: '45%',
+    height: '83%',
+    background: `url(${HeroImageLg})`,
+    backgroundRepeat: 'no-repeat',
+    backgroundSize: 'cover',
+    borderRadius: '42px 42px 0px 0px',
   },
   imageIcon: {
     height: '75px',
@@ -31,23 +34,7 @@ const useStyles = makeStyles({
 
 })
 
-function CatIcon() {
-  const classes = useStyles();
-
-  return (
-    <Icon className={classes.iconRoot} >
-      <img className={classes.imageIcon} src={CatWikiSVG} alt="cat logo"/>
-    </Icon>
-  )
-}
-
-function HomeIcon(props) {
-  return (
-    <SvgIcon {...props}>
-      <path d="M120.65 25.886L119.61 29.0067L118.57 32.9596C118.57 32.9596 115.865 34.416 115.657 33.3757C115.449 32.3355 116.697 32.1274 116.697 32.1274L117.113 30.047L113.993 24.4297L120.65 25.886Z" fill="#291507"/>
-    </SvgIcon>
-  );
-}
+//#291507
 
 function App() {
   
@@ -56,9 +43,9 @@ function App() {
   return (
     <Box className={classes.root}>
 
-      <Box>
-          <HomeIcon style={{fontSize: '100px', color:'white'}}/>
-          <Typography variant="p">Get to know more about cat breed</Typography>
+      <Box className={classes.upperRoot}>
+          <CatWiki fill='white'/>
+          <Typography variant="p" style={{color: 'whitesmoke'}}>Get to know more about cat breed</Typography>
       </Box>
 
       <Box>
